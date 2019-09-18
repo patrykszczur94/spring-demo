4d1303bc829c88cb8f1afa708e488df8a4448186
Papryk <patryk.szczur@agora.pl>
2019-09-18 12:14:16 +0200

    test

--- src/test/java/info/solidsoft/training/jenkins/springdemo/SpringDemoApplicationTests.java
+++ src/test/java/info/solidsoft/training/jenkins/springdemo/SpringDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package info.solidsoft.training.jenkins.springdemo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class SpringDemoApplicationTests {
 
     @Test
     public void contextLoads() {
+        int i = 2;
+        Assertions.assertThat(i).isEqualTo(3);
     }
 
 }
